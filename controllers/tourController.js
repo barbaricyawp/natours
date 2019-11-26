32a4f7298c4c5b1b91fbd36eccdbd49093fc3543
barbaricyawp <barbaricyawp24@gmail.com>
2019-11-26 14:26:50 -0600

    add filtering to search results

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,23 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    console.log(req.query);
+    // BUILD QUERY
+    // 1) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
+    // 2) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    console.log(JSON.parse(queryStr));
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -12,7 +27,8 @@ exports.getAllTours = async (req, res) => {
       }
     });
   } catch (err) {
-    res.send(404).json({
+    console.log(err);
+    res.status(404).json({
       status: 'fail',
       message: err
     });
@@ -24,7 +40,7 @@ exports.getTour = async (req, res) => {
     const tour = await Tour.findById(req.params.id);
     // const tour = await Tour.findOne({ _id: req.params.id });
 
-    res.status(200).json({
+    res.send(200).json({
       status: 'success',
       data: {
         tour
@@ -45,14 +61,14 @@ exports.createTour = async (req, res) => {
 
     const newTour = await Tour.create(req.body);
 
-    res.status(201).json({
+    res.send(201).json({
       status: 'success',
       data: {
         tour: newTour
       }
     });
   } catch (err) {
-    res.status(400).json({
+    res.send(400).json({
       status: 'fail',
       message: err
     });
@@ -65,14 +81,14 @@ exports.updateTour = async (req, res) => {
       new: true,
       runValidators: true
     });
-    res.status(200).json({
+    res.send(200).json({
       status: 'success',
       data: {
         tour
       }
     });
   } catch (err) {
-    res.status(400).json({
+    res.send(400).json({
       status: 'fail',
       message: 'Invalid data sent!'
     });
@@ -82,12 +98,12 @@ exports.updateTour = async (req, res) => {
 exports.deleteTour = async (req, res) => {
   try {
     await Tour.findByIdAndDelete(req.params.id);
-    res.status(204).json({
+    res.send(204).json({
       status: 'success',
       data: null
     });
   } catch (err) {
-    res.status(404).json({
+    res.send(404).json({
       status: 'fail',
       message: err
     });
